dc2e455bde51fe90aaebb9f5dbeee5efde9c37b2
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-05 16:11:23 +0200

    valore/funzionamento form

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole: - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+// selezione input
+const nameInput = document.querySelector(".nameinput");
+console.log(nameInput);
+// selezione button
+const btGenera = document.querySelector(".genera");
+console.log(btGenera);
+
+// gestione dell'interazione dell'utente
+btGenera.addEventListener("click",
+    function () {
+        let inputValue = nameInput.value;
+        console.log(inputValue);
+    }
+)
